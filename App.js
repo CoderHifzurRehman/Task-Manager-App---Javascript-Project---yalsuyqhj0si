587d1231e4671b4b8c7f29044db9389c515fbfe8
CoderHifzurRehman <120785266+CoderHifzurRehman@users.noreply.github.com>
2023-06-13 19:12:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,12 +7,15 @@ let draggableTodo = null;
 todos.forEach((todo) => {
   todo.addEventListener("dragstart", dragStart);
   todo.addEventListener("dragend", dragEnd);
+  todo.addEventListener("touchstart", touchStart);
+  todo.addEventListener("touchend", touchEnd);
 });
 
 function dragStart() {
+  // e.dataTransfer.setData("text/plain", null);
   draggableTodo = this;
-  progress = this.document.querySelector(".progress");
-  console.log(progress);
+  // progress = this.document.querySelector(".progress");
+  // console.log(progress);
   setTimeout(() => {
     this.style.display = "none";
   }, 0);
@@ -26,6 +29,77 @@ function dragEnd() {
   }, 0);
   console.log("dragEnd");
 }
+
+function touchStart(e) {
+  e.preventDefault();
+  draggableTodo = this;
+  setTimeout(() => {
+    this.style.opacity = "0.3";
+  }, 0);
+  console.log("touchStart");
+  this.addEventListener("touchmove", touchMove);
+}
+
+function touchMove(e) {
+  e.preventDefault();
+  console.log("move");
+}
+
+function touchEnd(e) {
+  e.preventDefault();
+  draggableTodo.style.transform = "none";
+    const targetStatus = getTargetStatus(e.changedTouches[0]);
+    if (targetStatus) {
+      targetStatus.appendChild(draggableTodo);
+      progress = draggableTodo.querySelector(".progress");
+
+      if (targetStatus == open_status) {
+        draggableTodo.style.borderLeft = "5px solid red";
+        progress.style.width = "25%";
+        progress.style.setProperty("--my-end-width", "25%");
+        progress.style.backgroundColor = "red";
+      } else if (targetStatus == progress_status) {
+        draggableTodo.style.borderLeft = "5px solid orange";
+        progress.style.width = "50%";
+        progress.style.setProperty("--my-end-width", "50%");
+        progress.style.backgroundColor = "orange";
+      } else if (targetStatus == review_status) {
+        draggableTodo.style.borderLeft = "5px solid yellow";
+        progress.style.width = "75%";
+        progress.style.setProperty("--my-end-width", "75%");
+        progress.style.backgroundColor = "yellow";
+      } else if (targetStatus == done_status) {
+        draggableTodo.style.borderLeft = "5px solid green";
+        progress.style.width = "100%";
+        progress.style.setProperty("--my-end-width", "100%");
+        progress.style.backgroundColor = "green";
+      }
+      console.log("dropped");
+    }
+  draggableTodo = null;
+  setTimeout(() => {
+    this.style.opacity = "1";
+  }, 0);
+  console.log("touchEnd");
+  this.removeEventListener("touchmove", touchMove);
+}
+
+function getTargetStatus(touch) {
+  const x = touch.clientX;
+  const y = touch.clientY;
+
+  for (let i = 0; i < all_status.length; i++) {
+    const status = all_status[i];
+    const rect = status.getBoundingClientRect();
+    if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+      return status;
+    }
+  }
+  return null;
+}
+
+//
+
 
 all_status.forEach((status) => {
   status.addEventListener("dragover", dragOver);
@@ -52,20 +126,55 @@ function dragLeave() {
 function dragDrop() {
   this.style.border = "none";
   this.appendChild(draggableTodo);
-  progress = draggableTodo.children[0].children[0];;
+  progress = draggableTodo.children[0].children[0];
+
   if(this==open_status){
+    draggableTodo.style.borderLeft = "5px solid red";
+    
     progress.style.width ="25%";
+    progress.style.setProperty('--my-end-width', '25%');
+    // getComputedStyle(document.documentElement).getPropertyValue('--my-start-width');
+    progress.style.backgroundColor = "red";
   }
   else if(this==progress_status){
+    draggableTodo.style.borderLeft = "5px solid orange";
     progress.style.width ="50%";
+    progress.style.setProperty('--my-end-width', '50%');
+    // getComputedStyle(document.documentElement).getPropertyValue('--my-start-width');
+    progress.style.backgroundColor = "orange";
   }
   else if(this==review_status){
+    draggableTodo.style.borderLeft = "5px solid yellow";
     progress.style.width ="75%";
+    progress.style.setProperty('--my-end-width', '75%');
+    // getComputedStyle(document.documentElement).getPropertyValue('--my-start-width');
+    progress.style.backgroundColor = "yellow";
   }
   else if(this==done_status){
+    draggableTodo.style.borderLeft = "5px solid green";
     progress.style.width ="100%";
+    progress.style.setProperty('--my-end-width', '100%');
+    // getComputedStyle(document.documentElement).getPropertyValue('--my-start-width');
+    progress.style.backgroundColor = "green";
   }
   console.log("dropped");
+}
+
+
+
+
+const deleteAll = document.getElementById("delete_btn");
+
+deleteAll.addEventListener("click", clearAllTasks);
+
+function clearAllTasks(){
+  const doneSection = document.getElementById("done_status");
+  const doneTasks = doneSection.querySelectorAll(".todo");
+  
+    doneTasks.forEach((doneTodos)=>{
+      doneTodos.remove();
+    });
+  
 }
 
 /* Modal */
@@ -83,7 +192,7 @@ btns.forEach((btn) => {
 
 close_modals.forEach((btn) => {
   btn.addEventListener("click", () => {
-    const modal = btn.closest(".modal");
+    const modal = btn.closest(".modals");
     modal.classList.remove("active");
     overlay.classList.remove("active");
   });
@@ -103,14 +212,25 @@ const todo_submit = document.getElementById("todo_submit");
 todo_submit.addEventListener("click", createTodo);
 
 function createTodo() {
+  const input_val = document.getElementById("todo_input").value;
+  const input_val2 = document.getElementById("descn_input").value;
+
+  if(input_val.length<3){
+    alert("Type Atleast 3 Characters Of Task Title");
+    return;
+  }
+  if(input_val2.length<6){
+    alert("Type Atleast 6 Characters Of Task Discription");
+    return;
+  }
+
   const todo_div = document.createElement("div");
   const todo_p = document.createElement("p");
   const todo_h = document.createElement("h3");
   const todo_progressBar = document.createElement("div");
   const todo_progress = document.createElement("div");
   
-  const input_val = document.getElementById("todo_input").value;
-  const input_val2 = document.getElementById("descn_input").value;
+  
   const txt = document.createTextNode(input_val);
   const txt2 = document.createTextNode(input_val2);
 
@@ -154,10 +274,14 @@ function createTodo() {
   span.addEventListener("click", () => {
     span.parentElement.style.display = "none";
   });
+
+
   //   console.log(todo_div);
 
   todo_div.addEventListener("dragstart", dragStart);
   todo_div.addEventListener("dragend", dragEnd);
+  todo_div.addEventListener("touchstart", touchStart);
+  todo_div.addEventListener("touchend", touchEnd);
 
   document.getElementById("todo_input").value = "";
   document.getElementById("descn_input").value = "";
